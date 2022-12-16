7d35bdf534f95dc3f53c306445fa2aefb6b1d0a0
Diego <diego.nathanael62@aluno.ifce.edu.br>
2022-12-16 12:05:56 -0300

    Nota 10

--- sgt/sgt/urls.py
+++ sgt/sgt/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("hello/", include("hello.urls"))
+	path("admin/", admin.site.urls),
+	path("hello/", include("hello.urls")), 
+	path('newyear/', include("newyear.urls")),
+	path('tasks/', include("tasks.urls"))
 ]
